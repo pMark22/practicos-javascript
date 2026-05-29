6b64c365d6df6f83866d5174b24477df31b93b20
pmark22 <elpachano202@gmail.com>
2026-05-29 10:51:02 -0300

    condicionales

---
+++ Javascript/actividad-3.js
@@ -0,0 +1,14 @@
+// Actividad 3. Clasificación por edad
+// Consigna: Crear una variable edad y mostrar "Niño" si es menor de 13, "Adolescente" si tiene
+// entre 13 y 17, y "Adulto" si tiene 18 o más.
+// Objetivo: Practicar if, else if y else.
+
+let edad = 16
+
+if (edad < 13) {
+    console.log("Niño")
+} else if (edad > 13 && edad < 17) {
+    console.log("Adolecente")
+} else {
+    console.log("Adulto")
+}
