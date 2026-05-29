93a38dd7bdeed60a457ab0f861a96ba1ed790dc2
pmark22 <elpachano202@gmail.com>
2026-05-29 12:21:28 -0300

    par/impar

---
+++ Javascript/actividad-6.js
@@ -0,0 +1,15 @@
+// Actividad 6. Pares e impares
+// Consigna: Recorrer los números del 1 al 20 y mostrar si cada número es par o impar.
+// Objetivo: Usar módulo y condicionales.
+// Pista: Recordá que numero % 2 === 0 permite saber si un número es par.
+
+
+for (let i = 0; i <= 20; i++) {
+
+    if (i % 2 === 0) {
+            console.log(`${i} PAR`);
+        } else {
+            console.log(`${i} INPAR`);
+        }
+
+}
