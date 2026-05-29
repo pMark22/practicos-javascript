b96135b7f677a880b9ece4177ae5b8e3ea849470
pmark22 <elpachano202@gmail.com>
2026-05-29 10:35:18 -0300

    condicionales

---
+++ Javascript/actividad-2.js
@@ -0,0 +1,12 @@
+// Actividad 2. Aprobado o desaprobado
+// Consigna: Creá una variable nota. Si es mayor o igual a 6, mostrar "Aprobado". Si no, mostrar
+// "Desaprobado".
+// Objetivo: Aplicar condicionales.
+
+let nota = 6
+
+if (nota >= 6) {
+    console.log(`Aprobado`)
+} else {
+    console.log(`Desaprobado`)
+}
