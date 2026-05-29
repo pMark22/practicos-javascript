170754fb72aca35733754b415804bbe45c6d6050
pmark22 <elpachano202@gmail.com>
2026-05-29 11:50:49 -0300

    for suma 1 al 100

---
+++ Javascript/actividad-5.js
@@ -0,0 +1,13 @@
+//Actividad 5. Suma acumulada
+//Consigna: Sumar los números del 1 al 100 y mostrar el resultado final.
+// Objetivo: Practicar variables acumuladoras.
+// Pista: Necesitás una variable para acumular el resultado y un for para recorrer los números.
+
+let sumaConstante = 0;
+
+for (let i = 0; sumaConstante < 100; i++) {
+    sumaConstante += 1
+    if (sumaConstante<=100){
+        console.log(sumaConstante)
+    }
+}
