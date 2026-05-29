c52b97f52c3d0871dbdafcddf1fca5897a801e1e
pmark22 <elpachano202@gmail.com>
2026-05-29 11:28:31 -0300

    for para multiplicar del 1 al 10

---
+++ Javascript/actividad-4.js
@@ -0,0 +1,10 @@
+// Actividad 4. Tabla de multiplicar
+// Consigna: Crear una variable numero y mostrar su tabla de multiplicar del 1 al 10.
+// Objetivo: Practicar bucles for.
+
+let numero = 9;
+
+for (let i = 0; i < 11; i++) {
+    console.log(numero * i);
+    
+}
